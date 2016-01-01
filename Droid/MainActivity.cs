211cbc161ea4e120ec0ea0cb91f9b602d3b246f3
René Ruppert <rene.ruppert@gmail.com>
2016-01-01 20:41:01 +0100

    Add Android version (no images yet)

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+
+namespace ETA.Droid
+{
+	[Activity (Label = "ETA.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			// Code for starting up the Xamarin Test Cloud Agent
+			#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start ();
+			#endif
+
+			var databasePath = Path.Combine (System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal));
+			if (!Directory.Exists (databasePath))
+			{
+				Directory.CreateDirectory (databasePath);
+			}
+
+			Console.WriteLine ($"Android database location: {databasePath}");
+			var fullPath = Path.Combine (databasePath, "data.sqlite");
+
+			var formsApp = new App (fullPath, new PlatformSpecific ());
+			LoadApplication (formsApp);
+		}
+	}
+}
+
---
+++ Droid/PlatformServices.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EtaShared;
+using AndroidHUD;
+
+namespace ETA.Droid
+{
+	public class PlatformSpecific : IPlatformSpecific
+	{
+		CancellationTokenSource cts;
+
+		public void DismissProgressIndicator()
+		{
+			if (AndHUD.Shared.CurrentDialog == null)
+			{
+				return;
+			}
+			AndHUD.Shared.Dismiss();
+		}
+
+		public CancellationToken ShowProgressIndicator(string msg, string cancel)
+		{
+			if(this.cts == null)
+			{
+				this.cts = new CancellationTokenSource();
+			}
+
+			if (AndHUD.Shared.CurrentDialog  != null)
+			{
+				return this.cts.Token;
+			}
+
+			if (string.IsNullOrWhiteSpace(cancel))
+			{
+				AndHUD.Shared.Show(Xamarin.Forms.Forms.Context, msg, -1, MaskType.Black);
+			}
+			else
+			{
+				AndHUD.Shared.Show(Xamarin.Forms.Forms.Context, msg, -1, MaskType.Black, default(TimeSpan?), null, true, () => {
+					this.cts.Cancel();
+					this.cts = null;
+				});
+			}
+			return this.cts.Token;
+		}
+	}
+}
